33fd23dd24f2562e03fa805501f2a41ae357f189
Xen0Xys <red57101@gmail.com>
2022-12-22 15:26:34 +0100

    :rocket: Add route to update a team score

---
+++ src/routes/teams/score/updateScore.js
@@ -0,0 +1,34 @@
+const auth = require ("../../../middlewares/authentication");
+const {models} = require("../../../database/sequelize");
+
+module.exports = (app) => {
+    app.post('/api/teams/score', auth, (req, res) => {
+        if(req.user.hasPassword === false) return res.status(403).json({message: "You need password authorization to do that"});
+        if(!req.body.teamCode || !req.body.newScore) return res.status(400).json({message: "Missing parameters"});
+        const teamCode = req.body.teamCode;
+        const newScore = req.body.newScore;
+        models.Team.findOne({where: {code: teamCode}}).then(team => {
+            if(!team) return res.status(404).json({message: "Team not found"});
+            const oldScore = team.score;
+            team.score = newScore;
+            team.save().then(() => {
+                // Send notification to all team members
+                models.User.findAll({where: {teamCode: teamCode}}).then(async users => {
+                    for(let user of users) {
+                        await models.Notification.create({
+                            senderId: req.user.id,
+                            receiverId: user.id,
+                            type: "score",
+                            jsonContent: {
+                                teamCode: teamCode,
+                                newScore: newScore,
+                                oldScore: oldScore
+                            }
+                        });
+                    }
+                    res.status(200).json({message: "Score updated"});
+                });
+            });
+        });
+    });
+}
