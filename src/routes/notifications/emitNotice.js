7632e91e972680579e4e79d8741281a33ba92ea5
Xen0Xys <red57101@gmail.com>
2022-12-22 14:36:00 +0100

    :rocket: Add custom pre-made message when fetching a notice

--- src/routes/notifications/emitNotice.js
+++ src/routes/notifications/emitNotice.js
@@ -2,19 +2,14 @@ const auth = require ("../../middlewares/authentication");
 const {models} = require("../../database/sequelize");
 
 module.exports = (app) => {
-    app.post('/api/notifications', auth, (req, res) => {
+    app.post('/api/notifications', auth, async (req, res) => {
         const receiverId = req.body.targetId;
         if(!receiverId) return res.status(400).json({message: "Missing targetId"});
         const senderId = req.user.id;
         if(receiverId === senderId) return res.status(400).json({message: "You can't send a notification to yourself"});
         const type = req.body.type;
-        if(!["info", "score", "invite", "join", "leave"].includes(type)) return res.status(400).json({message: "Invalid type"});
         const content = req.body.content;
-        // Check content to verify if all require fields dot each type are present
-        if(type === "info" && !content.message) return res.status(400).json({message: "Missing message field in content"});
-        if(type === "score" && (!content.oldScore || !content.newScore)) return res.status(400).json({message: "Missing oldScore or newScore fields in content"});
-        if(type === "invite" && !content.teamCode) return res.status(400).json({message: "Missing teamCode field in content"});
-        if((type === "join" || type === "leave") && (!content.teamCode || !content.targetId)) return res.status(400).json({message: "Missing teamCode or targetId field in content"});
+        if(await checkNotice(res, type, content) !== 0) return;
         models.User.findOne({where: {id: receiverId}}).then(user => {
             if(!user) return res.status(404).json({message: "Target user not found"});
             models.Notification.create({
@@ -32,4 +27,14 @@ module.exports = (app) => {
             res.status(500).json({message: "Error when searching for the target user", error: err});
         });
     });
+}
+
+async function checkNotice(res, noticeType, content){
+    if(!["info", "score", "invite", "join", "leave"].includes(noticeType)) return res.status(400).json({message: "Invalid type"});
+    // Check content to verify if all require fields dot each type are present
+    if(noticeType === "info" && !content.message) return res.status(400).json({message: "Missing message field in content"});
+    if(noticeType === "score" && (!content.oldScore || !content.newScore)) return res.status(400).json({message: "Missing oldScore or newScore fields in content"});
+    if(noticeType === "invite" && !content.teamCode) return res.status(400).json({message: "Missing teamCode field in content"});
+    if((noticeType === "join" || noticeType === "leave") && (!content.teamCode || !content.targetId)) return res.status(400).json({message: "Missing teamCode or targetId field in content"});
+    return 0;
 }
--- src/routes/notifications/getNotices.js
+++ src/routes/notifications/getNotices.js
@@ -4,12 +4,44 @@ const auth = require ("../../middlewares/authentication");
 module.exports = (app) => {
     app.get('/api/notifications', auth, (req, res) => {
         const userId = req.user.id;
-        models.Notification.findAll({where: {receiverId: userId}}).then(notices => {
+        models.Notification.findAll({where: {receiverId: userId}}).then(async notices => {
             if(notices.length === 0) return res.status(404).json({message: "No notices found"});
-            res.status(200).json(notices);
+            const finalNotices = [];
+            for(let notice of notices){
+                notice = notice.toJSON();
+                notice.jsonContent.message = await getMessage(notice.type, notice.jsonContent);
+                finalNotices.push(notice);
+            }
+            res.status(200).json(finalNotices);
         }).catch(err => {
             console.log(err)
             res.status(500).json({message: "Internal server error", error: err});
         });
     });
+}
+
+async function getMessage(noticeType, content){
+    switch (noticeType) {
+        case "info": {
+            return content.message;
+        }
+        case "score": {
+            return "Votre score est passé de " + content.oldScore + " points à " + content.newScore + " points !";
+        }
+        case "invite": {
+            const teamName = (await models.Team.findOne({where: {code: content.teamCode}})).name;
+            return "Vous avez été invité à rejoindre l'équipe " + teamName + " !";
+        }
+        case "join": {
+            const userName = (await models.User.findOne({where: {id: content.targetId}})).username;
+            return userName + " a rejoint votre équipe !";
+        }
+        case "leave": {
+            const userName = (await models.User.findOne({where: {id: content.targetId}})).username;
+            return userName + " a quitté votre équipe !";
+        }
+        default: {
+            return "Unknown notification type";
+        }
+    }
 }
