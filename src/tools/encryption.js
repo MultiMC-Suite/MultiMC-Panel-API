4e4378b5b64ead32bce43f4643398fabda04dfb0
Xen0Xys <red57101@gmail.com>
2022-12-22 14:44:24 +0100

    :bug: Remove warning with unused code

--- src/tools/encryption.js
+++ src/tools/encryption.js
@@ -1,6 +1,5 @@
 const bcrypt = require(`bcrypt`);
 const jwt = require(`jsonwebtoken`);
-const {models} = require("../database/sequelize");
 
 const encrypt = async (password) => {
     return await bcrypt.hash(password, 10);
