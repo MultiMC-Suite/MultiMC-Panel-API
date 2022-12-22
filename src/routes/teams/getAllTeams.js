1c197aecb2cc0470d1bdfd971e5c6d2b7125c036
Xen0Xys <red57101@gmail.com>
2022-12-22 11:03:52 +0100

    :rocket: Add url param "complete" to get a more complete data for teams (owner and team members as user objects)

--- src/routes/teams/getAllTeams.js
+++ src/routes/teams/getAllTeams.js
@@ -5,7 +5,34 @@ module.exports = (app) => {
     app.get("/api/teams", auth, (req, res) => {
         models.Team.findAll().then(teams => {
             if(teams === null) return res.status(404).json({message: "No teams found"});
-            return res.status(200).json(teams);
+            if(req.query.complete !== "id" && req.query.complete !== "users") return res.status(200).json(teams);
+            models.User.findAll().then(users => {
+                const teamsWithUsers = [];
+                for(let team of teams){
+                    team = team.toJSON();
+                    const members = [];
+                    const teamMembers = users.filter(user => user.teamCode === team.code);
+                    for(let user of teamMembers){
+                        user = user.toJSON();
+                        delete user.password;
+                        delete user.teamCode;
+                        delete user.groupId;
+                        if(user.id === team.ownerId){
+                            if(req.query.complete === "users")
+                                team.owner = user;
+                        }else{
+                            if(req.query.complete === "users")
+                                members.push(user);
+                            else
+                                members.push(user.id);
+                        }
+                    }
+                    team.members = members;
+                    console.log(team);
+                    teamsWithUsers.push(team);
+                }
+                return res.status(200).json(teamsWithUsers);
+            });
         }).catch(error => {
             return res.status(500).json({message: "Error with database", data: error});
         });
